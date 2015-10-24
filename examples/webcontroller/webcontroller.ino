06b784c8438e22f8e6c3a2b87d127de403d5e8c9
Karan Purohit <karnpurohit@gmail.com>
2015-10-24 12:44:53 +0530

    updated example

--- examples/webcontroller/webcontroller.ino
+++ examples/webcontroller/webcontroller.ino
@@ -1,22 +1,28 @@
-#include<Angduino.h>
+/*
+    This is an simple example showing how to use the Angduino Library.
+*/
 
-Angduino angduino;
+#include<Angduino.h>  //including the library
+
+Angduino angduino;    //creating the object
 void setup()
 {
 
-  Serial.begin(9600);
-  Serial.println("Hello Terminal");
+  Serial.begin(9600);                  //You set this up your sell
+  //Important: DO NOT USE the Serial function from this point onwards.
+  //Angduino take the control if Serial channels for communication.
+
   //NOTE: Always call setKey in the setup. Should be called only once.
-  angduino.setKey("led1");
-    delay(1000);
+  angduino.setKey("led2");            //Setting the key.Default allowed keys: 50
+  delay(1000);                        //Its recommended that you set this delay for proper start up
+
   pinMode(13, OUTPUT);           // set pin to input
 }
 
 void loop ()
 {
-  angduino.update();
-  delay(100);
-  if(angduino.getValue("led1")==1)
+  angduino.update();   //necessary, otherwise changes wont be Updated.
+  if(angduino.getValue("led2")==1)    //get the value from the key.
     digitalWrite(13,HIGH);
   else
     digitalWrite(13,LOW);
